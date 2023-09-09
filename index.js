d534b47174c3737e3605b9fcdc8616fb6b2f1d98
Gaurav <gk44035@gmail.com>
2023-09-10 01:32:55 +0530

    replaced bodyparser

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require("./db/db")
 const express = require("express")
-const bodyParser = require("body-parser")
 const userroutes = require("./routes/userRoutes");
 const fileroutes = require("./routes/fileRoutes")
+// const passport = require('passport')
 const app= express();
 const PORT = 3091
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.urlencoded({extended:false}))
 app.use('/api/file',fileroutes)
 app.use('/api',userroutes);
