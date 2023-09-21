9ab7ed188b77c05727664846ca4e38a96843d3ef
Gaurav <gk44035@gmail.com>
2023-09-21 15:59:00 +0530

    yml code for every file routes

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -13,8 +13,76 @@ const storage = multer.diskStorage({
   })
 const upload = multer({storage})
 
+/**
+ * @swagger
+ * /api/file/upload:
+ *   post:
+ *     summary: Upload a file
+ *     description: Upload a file to the server using FormData.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       200:
+ *         description: File uploaded successfully
+ *       400:
+ *         description: Bad request, missing or invalid file
+ *       500:
+ *         description: Internal server error
+ *     tags:
+ *       - File
+ */
 filerouter.post('/upload',upload.single("file"),fileUpload)
+
+/**
+ * @swagger
+ * /api/file/getfiles:
+ *   get:
+ *     summary: Get a list of files
+ *     description: Retrieve a list of files.
+ *     responses:
+ *       200:
+ *         description: List of files retrieved successfully
+ *       500:
+ *         description: Internal server error
+ *     tags:
+ *       - File
+ */
+
 filerouter.get('/getfiles',getFile)
+
+/**
+ * @swagger
+ * /api/file/deletefile/{id}:
+ *   delete:
+ *     summary: Delete file information by using ID
+ *     description: Delete file information 
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: The ID of the file to update.
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: file deleted successfully
+ *       400:
+ *         description: Bad request, missing or invalid data
+ *       404:
+ *         description: file not found
+ *       500:
+ *         description: Internal server error
+ *     tags:
+ *       - File
+ */ 
 filerouter.delete('/deletefile/:id',deleteFile);
 
 module.exports = filerouter
